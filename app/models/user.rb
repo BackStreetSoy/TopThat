f097c348fd616ff406518fd26a1cc824a1870bf4
BackStreetSoy <jordanedwardobey@gmail.com>
2016-02-06 10:39:52 -0600

    authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+    has_secure_password 
+    
     has_many :videos
     has_many :challenges, through: :videos
     has_many :friendships 
     has_many :friends, through: :friendships, source: :friend
-    #determine whether to encapsulate friends in method or to produce seperate friendship records 
     has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
     has_many :inverse_friends, through: :inverse_friendships, source: :user
     has_many :comments, foreign_key: :commenter_id
@@ -15,8 +16,6 @@ class User < ActiveRecord::Base
     has_many :challenges_as_opponent, through: :instances_as_a_direct_opponent, source: :challenge 
     has_many :votes, foreign_key: :voter_id
     has_many :voted_on_videos, through: :votes
-    #test opponents association
-    #output should be array of all direct opponents 
     has_many :opponents, through: :challenges
     has_many :replies, foreign_key: :replier_id
 
