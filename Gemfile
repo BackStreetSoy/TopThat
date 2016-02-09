65115ac19ca44cb15b20858d89446b319645b516
BackStreetSoy <jordanedwardobey@gmail.com>
2016-02-09 16:20:42 -0600

    added rack-cors gem

--- Gemfile
+++ Gemfile
@@ -24,10 +24,11 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'faker'
+gem 'rack-cors', :require => 'rack/cors'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-gem 'active_model_serializer'
+gem 'active_model_serializers'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def index 
+        @user = User.all 
+    end 
+
+    def new
+        @user = User.new 
+    end 
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+        else 
+            flash[:error] = "incorrect email or password"
+            redirect_to root_path
+        end 
+    end
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password, :about_me, :wins, :losses, :ties)
+    end 
 end
